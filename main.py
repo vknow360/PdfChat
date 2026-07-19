e2f2c991b24ed593c585718daa11fb69dcc9ed7d
Sunny Gupta <vknow360@gmail.com>
2026-07-19 18:00:22 +0530

    feat: implement FastAPI application for PDF interaction with memory management and session support

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
+from uvicorn import run
 
 load_dotenv()
 
 if __name__ == "__main__":
-    from src.app import run_app
-    run_app()
+    run("src.app:app", host="127.0.0.1", port=8000, reload=True)
--- src/app.py
+++ src/app.py
@@ -1,82 +1,75 @@
+import uuid
+import hashlib
+import threading
 from src.chains import build_chat_chain, format_messages
 from src.chains import build_summary_chain
 from src.config import chat_model, embeddings, BASE_DIR
 from src.prompts import query_prompt, summary_prompt
 from langchain_core.output_parsers import StrOutputParser
 import os
-import streamlit as st
+from fastapi import FastAPI, UploadFile
 
 from src.ingestion import create_retriever
 
 UPLOAD_DIR = os.path.join(BASE_DIR, "uploads")
 
-@st.cache_resource
-def get_model():
-    return chat_model
+lock_a = threading.Lock()
 
-@st.cache_resource
-def get_embeddings():
-    return embeddings
+conversations = {}
 
-model = get_model()
-embeddings = get_embeddings()
+class Conversation():
+    def __init__(self, file_hash):
+        self.id = uuid.uuid4().__str__()
+        self.messages = []
+        self.file_hash = file_hash
+        self.retriever = create_retriever(os.path.join(UPLOAD_DIR, f"{file_hash}.pdf"), embeddings)
+
+    def __str__(self):
+        return self.id
 
 parser = StrOutputParser()
 
 
-def run_app():
-    st.title("PDF Chatbot")
+app = FastAPI()
 
-    uploaded_file = st.file_uploader("Upload pdf", type="pdf")
-    if uploaded_file:
-        if "last_file" not in st.session_state:
-            st.session_state.last_file = None
-        
-        if uploaded_file.name != st.session_state.last_file:
-            try:
-                with st.spinner("Processing PDF..."):
-                    os.makedirs(UPLOAD_DIR, exist_ok=True)
-                    save_path = os.path.join(UPLOAD_DIR, uploaded_file.name)
-                    with open(save_path, "wb") as f:
-                        f.write(uploaded_file.getvalue())
-                    st.session_state.retriever = create_retriever(save_path, embeddings)
-                st.session_state.last_file = uploaded_file.name
-            except Exception as e:
-                st.error(f"Error processing file: {e}")
+@app.post("/upload")
+def upload_file(file: UploadFile):
+    os.makedirs(UPLOAD_DIR, exist_ok=True)
+    data = file.file.read()
+    file_hash = hashlib.md5(data).hexdigest()
+    file_path = os.path.join(UPLOAD_DIR, f"{file_hash}.pdf")
+    
+    with open(file_path, "wb") as f:
+        f.write(data)
 
-    if "retriever" in st.session_state:
-        if "messages" not in st.session_state:
-            st.session_state.messages = []
+    conversation = Conversation(file_hash)
+    conversations[conversation.id] = conversation
+    return conversation.id
 
-        for msg in st.session_state.messages:
-            st.chat_message(msg["role"]).write(msg["content"])
-        
-        retriever = st.session_state.retriever
-        
-        query = st.chat_input("Ask question from pdf")
-        if query:
-            st.session_state.messages.append({"role": "user", "content" : query})
-            st.chat_message("user").write(query)
+@app.post("/chat/{id}")
+def chat(id: str, msg: str):
+    conversation = conversations.get(id)
+    if not conversation:
+        return {"error": "Conversation not found"}
 
-            with st.spinner("Generating Response"):
+    with lock_a:
+        if len(conversation.messages) > 20:
+            summary_chain = build_summary_chain(summary_prompt, chat_model, parser)
+            summary_content = summary_chain.invoke({
+                "text": format_messages(conversation.messages[:-10])
+            })
+            conversation.messages = [
+                {"role": "system", "content": summary_content} , *conversation.messages[-10:]
+            ]
 
-                if len(st.session_state.messages) > 20:
-                    
-                    summary_chain = build_summary_chain(summary_prompt, model, parser)
-                    summary_content = summary_chain.invoke({
-                        "text": format_messages(st.session_state.messages[:-10])
-                    })
-                    st.session_state.messages = [
-                        {"role": "assistant", "content": summary_content} , *st.session_state.messages[-10:]
-                    ]
+    conversation.messages.append({"role": "user", "content": msg})
 
-                query_chain = build_chat_chain(retriever, model, query_prompt)
-                answer_stream = query_chain.stream(
-                    {
-                        'question': query,
-                        'messages': st.session_state.messages
-                    }
-                )
-
-                response = st.chat_message("assistant").write_stream(answer_stream)
-                st.session_state.messages.append({"role": "assistant", "content" : response})
+    query_chain = build_chat_chain(conversation.retriever, chat_model, query_prompt)
+    answer = query_chain.invoke(
+        {
+            'question': msg,
+            'messages': conversation.messages
+        }
+    )
+    conversation.messages.append({"role": "assistant", "content" : answer})
+    return answer
